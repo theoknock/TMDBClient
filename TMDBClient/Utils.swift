b1b49ba0c591b5b87f0e6a8d996c1af24c63a0f0
James Alan Bush <jamesalanbush1972@gmail.com>
2021-04-03 19:28:53 -0400

    Initial commit of walk-through demo

---
+++ TMDBClient/Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Utils.swift
+//  TMDBClient
+//
+//  Created by Xcode Developer on 4/3/21.
+//
+
+import Foundation
+
+class Utils {
+    
+    static let jsonDecoder: JSONDecoder = {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return jsonDecoder
+    }()
+    
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-mm-dd"
+        return dateFormatter
+    }()
+}
