d3a4e034ee2188d9465546afbe2044aad967ad9e
James Alan Bush <jamesalanbush1972@gmail.com>
2021-04-03 18:49:49 -0400

    Initial Commit

---
+++ TMDBClient/TMDBClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBClientApp.swift
+//  TMDBClient
+//
+//  Created by Xcode Developer on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TMDBClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
